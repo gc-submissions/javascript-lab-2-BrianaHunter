68ddaaa3da62ecb5a66f969d61d521aaad7d3d3c
Briana Hunter <brianahunter@Brianas-MacBook-Pro.local>
2022-05-03 19:24:33 -0400

    created and tested randomDamage function and created and tested chooseOption fuction.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,18 @@
-"use strict";
+// "use strict";
 
 // TODO - write your code here.
+
+    const randomDamage = () => {
+        min = Math.ceil(1);
+        max = Math.floor(10);
+        return Math.floor(Math.random() * (max - min + 1));
+    };
+    console.log(randomDamage(10));
+
+    const chooseOption = (opt1, opt2) => {
+        let randNum = Math.random() * 1;
+        return randNum === 0
+        ? opt1
+        : opt2;
+    };
+    console.log(chooseOption('opt1', 'opt2'));
